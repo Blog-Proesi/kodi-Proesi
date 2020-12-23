33ccf48fe2f7ada9ee7a3fa248d9626db8552fc3
Blog-Proesi <76419007+Blog-Proesi@users.noreply.github.com>
2020-12-23 18:54:33 -0300

    proesi
    kodi

--- plugin.video.amazon-test/clean.bat
+++
@@ -1,1 +0,0 @@
-@%~dp0createxml.bat clean
--- repo/plugin.video.netflix-0.15.8/resources/lib/kodi/ui/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Kodi GUI stuff"""
-# pylint: disable=wildcard-import
-from __future__ import absolute_import, division, unicode_literals
-
-from .dialogs import *
-from .xmldialogs import *
